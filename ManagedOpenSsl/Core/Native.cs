272138508326aefeacdb8a1191ef0c5ac671d4fc
Simon Jackson <si@sonocent.com>
2016-03-03 10:41:11 +0000

    Modified to work with iOS static libraries.

--- ManagedOpenSsl/Core/Native.cs
+++ ManagedOpenSsl/Core/Native.cs
@@ -99,8 +99,8 @@ namespace OpenSSL.Core
 		/// This is the name of the DLL that P/Invoke loads and tries to bind all of
 		/// these native functions to.
 		/// </summary>
-		const string DLLNAME = "libeay32";
-		const string SSLDLLNAME = "ssleay32";
+		const string DLLNAME = "__Internal";
+		const string SSLDLLNAME = "__Internal";
 
 		#region Delegates
 
